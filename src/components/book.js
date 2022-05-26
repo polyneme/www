674adf99d20fc4a56c186cc9a467a8a1553c6145
Donny Winston <donny@polyneme.xyz>
2022-05-26 16:57:51 -0400

    change link

--- src/components/book.js
+++ src/components/book.js
@@ -2,8 +2,8 @@
 import { jsx, Container, Button } from "theme-ui"
 export default () => (
   <Container sx={{ width: "320px", mx: "auto" }}>
-    <a href="https://meet.polyneme.xyz/15-min-free">
-      <Button>Book a Free 15-minute Consultation</Button>
+    <a href="https://meet.polyneme.xyz/10-min-chat">
+      <Button>Book a Free 10-minute Consultation</Button>
     </a>
   </Container>
 )
