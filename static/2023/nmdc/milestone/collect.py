05ff8cffda313856c573fb6574633b5d9e0f374a
Donny Winston <donny@polyneme.xyz>
2023-01-09 15:42:10 -0500

    html reps

--- static/2023/nmdc/milestone/collect.py
+++ static/2023/nmdc/milestone/collect.py
@@ -3,6 +3,7 @@ import json
 import re
 from typing import List
 
+PATH_PREFIX = "https://www.polyneme.xyz/2023/nmdc/milestone/"
 
 def process_row(row: List[str], store: dict):
     header_prefix = {
@@ -18,31 +19,55 @@ def process_row(row: List[str], store: dict):
     for text, code in header_prefix.items():
         if label.startswith(text):
             store["_next_type"] = code
+            store["@graph"].append({
+                "@id": PATH_PREFIX + store["_next_type"],
+                "skos:prefLabel": label,
+            })
 
     if re.fullmatch(r"\d+\.\d+", label):
-        store[(store["_next_type"] + label)] = description
+        store["@graph"].append({
+            "@id": PATH_PREFIX + 'm' + label,
+            "skos:prefLabel": description,
+            "skos:broader": PATH_PREFIX + store["_next_type"]
+        })
 
 
 if __name__ == "__main__":
 
-    store = {}
-    namespace = "https://www.polyneme.xyz/2023/nmdc/milestone/"
+    store = {
+        "@context": "http://www.w3.org/2013/json-ld-context/rdfa11",
+        "@graph": [],
+    }
 
     with open("roadmap_FY23-25.tsv") as file_in:
         reader = csv.reader(file_in, delimiter='\t')
         for row in reader:
             process_row(row, store)
 
-    for k, v in store.items():
-        if k.startswith("_"):
-            continue
+    for doc in store["@graph"]:
+        local_name = doc["@id"].split("/")[-1]
+        with open(local_name + '.html', "w") as file_out:
+            doc |= {"@context": store["@context"]}
+            body = f"""\
+<!doctype html>
+<html lang="en">
+<head>
+<title>{doc["skos:prefLabel"]}</title>
+<meta charset="UTF-8">
+<script type="application/ld+json">
+{json.dumps(doc, indent=2)}
+</script>
+</head>
+<body>
+<pre>
+{json.dumps(doc, indent=2)}
+</pre>
+</body>
+</html>
+            """
+            body = re.sub(r'"(https?://[^\"]+)"', r'<a href="\1">\1</a>', body)
+            file_out.write(body)
 
-        with open(k, "w") as file_out:
-            json.dump({
-                "@context": "http://www.w3.org/2013/json-ld-context/rdfa11",
-                "@id": namespace + k,
-                "skos:prefLabel": v,
-            }, file_out, indent=2)
 
 
 
